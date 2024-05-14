3053f22c8bbdadd46cd23b0da8707da94e8fa6db
crystian lefundes <crystian.lf@gmail.com>
2024-05-14 20:18:03 -0300

    commit inicial

---
+++ src/main/java/br/com/cryslefundes/animais/AnimaisApplication.java
@@ -0,0 +1,13 @@
+package br.com.cryslefundes.animais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cryslefundes/animais/AnimaisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cryslefundes.animais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
